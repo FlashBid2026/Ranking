f3b507c91ba6d45fd15ca176f78acba1dff3bc11
junyeong <whwnsdud4592@naver.com>
2026-02-10 19:45:07 +0900

    bug: RabbitMQ에 queue가 생성되지 않으면 생성되게 수정

--- src/main/java/FlashBid_Ranking/Ranking/Config/RabbitConsumerConfig.java
+++ src/main/java/FlashBid_Ranking/Ranking/Config/RabbitConsumerConfig.java
@@ -1,5 +1,9 @@
 package FlashBid_Ranking.Ranking.Config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -11,6 +15,23 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConsumerConfig {
 
   public static final String QUEUE_NAME = "auction.ranking.queue";
+  public static final String EXCHANGE_NAME = "auction.exchange";
+  public static final String ROUTING_KEY = "auction.bid.#";
+
+  @Bean
+  public Queue rankingQueue() {
+    return new Queue(QUEUE_NAME, true);
+  }
+
+  @Bean
+  public TopicExchange auctionExchange() {
+    return new TopicExchange(EXCHANGE_NAME);
+  }
+
+  @Bean
+  public Binding binding(Queue rankingQueue, TopicExchange auctionExchange) {
+    return BindingBuilder.bind(rankingQueue).to(auctionExchange).with(ROUTING_KEY);
+  }
 
   @Bean
   public MessageConverter jsonMessageConverter() {
